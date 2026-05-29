9a4a1e4873857c242fc712bc57b97a70a5029916
karanveer91 <158732626+karanveer91@users.noreply.github.com>
2026-05-29 20:00:03 +0530

    new data

--- pratices_question_easy_to hard_level/veriable_easy_to_hard.py
+++ pratices_question_easy_to hard_level/veriable_easy_to_hard.py
@@ -201,6 +201,131 @@ reaming = minutes % 60
 print(f"hours {hours} and the {reaming} reaming") #output:- hours 2  0 reaming
 
 
+#Combine two string variables into one sentence.
+
+name1 = "my name is vianshh"
+name2 = "and i from india"
+
+print(name1 + " " + name2)
+
+#------------------------------------
+
+name1 = "vansh"
+name2 = "23"
+
+print(f"my name is {name1} i from india my age is {name2} years old")
+
+
+
+#Calculate area of a circle using variable radius.
+
+radius = 10
+
+area = 3.14 * (radius * radius)
+
+print(area) #output:- 314.0
+
+
+'''
+#  MEDIUM LEVEL
+'''
+
+
+#Store an amount and print final amount after GST.
+
+amount  = 300
+GST = 15
+
+final_amount = amount + (amount * GST / 100)
+
+print(final_amount) #output:- 345.0
+
+
+#Calculate simple interest using variables.
+
+money = 2000
+interest_rate = 10
+time_year = 1
+
+final_intrest = (money * interest_rate * time_year /100) #the formula of the simple interest
+
+print(final_intrest) #output:- 200.0 only simple interest rate in one years
+
+
+
+#Store a 5-digit number and print its last digit.
+
+number = "1 , 2 , 3 , 4 , 5"
+
+print(number[-1]) #output:- negative indexing is -1 the answer the (5)
+
+
+#Print first digit of a number stored in variable num.
+
+
+number = 1 , 2 , 3 , 4 , 5
+
+print(number[0]) #output:- 1
+
+#NUMBER = 12345
+#print(str(number) [0]) #convert the intger to string than answer the (1)
+
+
+
+#Compare two variables and store result in another variable.
+
+number1 = 10
+number2 = 10
+
+number = number1 == number2
+
+print(number) #output:- true
+
+
+#Store a string and save its length in another variable.
+
+name  = "my name is vanshhh and i am 23 years old"
+
+name1 = len(name)
+
+print(name1) #output:- 40
+
+
+#Store a name and convert it to uppercase.
+
+name = 'my name is vanshh and  am from india'
+
+name1 = name.upper() # inbuild function  in python
+
+print(name1) # MY NAME IS VANSHH AND  AM FROM INDIA
+
+name = "MY NAME IS VANSHH AND I AM FROM THE INDIA AND I 23 YERAS OLD "
+
+name1 = name.lower()  # inbuild function  in python
+
+print(name1) #my name is vanshh and i am from the india and i 23 yeras old 
+
+
+#Store a sentence and count total words.
+
+name = "my name is vanshh  and i am from the india and i 23 yeras old "
+
+name1 = len(name.split()) #Python me words count karne ke liye .split() use hota hai.
+print(name1)  #output:- 15
+
+
+#Store division, floor division, and modulus in separate variables.
+
+number = 11
+division = number / 10
+print(division)  #output:- 1.1
+
+floor_division = number // 10
+print(floor_division) #output:- 1
+
+modulus = number % 10
+print(modulus) #output:- 1
+
 
 
 
